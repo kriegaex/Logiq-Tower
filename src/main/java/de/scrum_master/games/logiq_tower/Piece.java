113d000da250020ba332896536b2016f4b886348
Alexander Kriegisch <Alexander@Kriegisch.name>
2015-05-15 15:15:44 +0200

    Add simpler (text-based) constructor, split central/outer pieces

--- src/main/java/de/scrum_master/games/logiq_tower/Piece.java
+++ src/main/java/de/scrum_master/games/logiq_tower/Piece.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Piece {
-	public static final List<Piece> PIECES = new ArrayList<>();
+	public static final List<Piece> CENTRAL_PIECES = new ArrayList<>();
+	public static final List<Piece> OUTER_PIECES   = new ArrayList<>();
 
 	private final char symbol;
 	private final boolean isCentral;
@@ -17,22 +18,22 @@ public class Piece {
 	private final int rows;
 
 	static {
-		PIECES.add(new Piece('0', true, new boolean[][] { { true,  true  } }));
-		PIECES.add(new Piece('1', true, new boolean[][] { { true,  false, true  } }));
-		PIECES.add(new Piece('2', true, new boolean[][] { { true,  false, false,  true  } }));
-		PIECES.add(new Piece('3', true, new boolean[][] { { true,  false, false,  false, true  } }));
-		PIECES.add(new Piece('4', true, new boolean[][] { { true,  false, false,  false, false, true  } }));
+		CENTRAL_PIECES.add(new Piece(true, "00"));
+		CENTRAL_PIECES.add(new Piece(true, "1 1"));
+		CENTRAL_PIECES.add(new Piece(true, "2  2"));
+		CENTRAL_PIECES.add(new Piece(true, "3   3"));
+		CENTRAL_PIECES.add(new Piece(true, "4    4"));
 
-		PIECES.add(new Piece('I', false, new boolean[][] { { true,  true,  true,  true,  true  } }));
-		PIECES.add(new Piece('Y', false, new boolean[][] { { false, false, true,  false }, { true,  true,  true,  true  } }));
-		PIECES.add(new Piece('L', false, new boolean[][] { { false, false, false, true  }, { true,  true,  true,  true  } }));
-		PIECES.add(new Piece('N', false, new boolean[][] { { false, false, true,  true  }, { true,  true,  true,  false } }));
-		PIECES.add(new Piece('U', false, new boolean[][] { { true,  false, true  }, { true,  true,  true  } }));
-		PIECES.add(new Piece('Q', false, new boolean[][] { { true,  true,  true  }, { true,  true,  false } }));
-		PIECES.add(new Piece('W', false, new boolean[][] { { false, false, true  }, { false, true,  true  }, { true,  true,  false } }));
-		PIECES.add(new Piece('F', false, new boolean[][] { { false, true,  false }, { true,  true,  true  }, { true,  false, false } }));
-		PIECES.add(new Piece('T', false, new boolean[][] { { true,  true,  true  }, { false, true,  false }, { false, true,  false } }));
-		PIECES.add(new Piece('S', false, new boolean[][] { { false, true,  true  }, { false, true,  false }, { true,  true,  false } }));
+		OUTER_PIECES.add(new Piece(false, "IIIII"));
+		OUTER_PIECES.add(new Piece(false, "  Y ,YYYY"));
+		OUTER_PIECES.add(new Piece(false, "   L,LLLL"));
+		OUTER_PIECES.add(new Piece(false, "  NN,NNN "));
+		OUTER_PIECES.add(new Piece(false, "U U,UUU"));
+		OUTER_PIECES.add(new Piece(false, "QQQ,QQ "));
+		OUTER_PIECES.add(new Piece(false, "  W, WW,WW "));
+		OUTER_PIECES.add(new Piece(false, " F ,FFF,F  "));
+		OUTER_PIECES.add(new Piece(false, "TTT, T , T "));
+		OUTER_PIECES.add(new Piece(false, " SS, S ,SS "));
 	}
 
 	public Piece(char symbol, boolean isCentral, boolean[][] shape) {
@@ -66,6 +67,24 @@ public class Piece {
 		shapeRotatedText = builder.toString();
 
 		isPointSymmetric = shapeText.equals(shapeRotatedText);
+	}
+
+	public Piece(boolean isCentral, String shapeText) {
+		this(shapeText.replaceAll("[ ,]", "").charAt(0), isCentral, textToShape(shapeText));
+	}
+
+	private static boolean[][] textToShape(String shapeText) {
+		String[] textRows = shapeText.split(",");
+		int rows = textRows.length;
+		int columns = textRows[0].length();
+		boolean[][] shape = new boolean[rows][columns];
+		for (int row = 0; row < rows; row++) {
+			for (int column = 0; column < columns; column++) {
+				char character = textRows[row].charAt(column);
+				shape[row][column] = character != ' ';
+			}
+		}
+		return shape;
 	}
 
 	public int getColumns() {
@@ -104,8 +123,18 @@ public class Piece {
 	}
 
 	public static void main(String[] args) {
-		for (Piece piece : PIECES) {
-			System.out.println(piece + "\n");
+		System.out.println("CENTRAL PIECES");
+		System.out.println("==============\n");
+		for (Piece piece : CENTRAL_PIECES) {
+//			System.out.println(piece + "\n");
+			System.out.println(piece.getShapeText());
+			if (!piece.isPointSymmetric())
+				System.out.println(piece.getShapeRotatedText());
+		}
+		System.out.println("OUTER PIECES");
+		System.out.println("=============\n");
+		for (Piece piece : OUTER_PIECES) {
+//			System.out.println(piece + "\n");
 			System.out.println(piece.getShapeText());
 			if (!piece.isPointSymmetric())
 				System.out.println(piece.getShapeRotatedText());
