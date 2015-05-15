71c7e029c7ef5aa26563a186b62f9475b55fb590
Alexander Kriegisch <Alexander@Kriegisch.name>
2015-05-15 13:22:39 +0200

    Cosmetics: remove super-class JavaDoc from IFSException

--- src/main/java/de/scrum_master/games/logiq_tower/IllegalFieldSizeException.java
+++ src/main/java/de/scrum_master/games/logiq_tower/IllegalFieldSizeException.java
@@ -1,14 +1,6 @@
 package de.scrum_master.games.logiq_tower;
 
 public class IllegalFieldSizeException extends Exception {
-	/**
-	 * Constructs a new exception with the specified detail message.  The
-	 * cause is not initialized, and may subsequently be initialized by
-	 * a call to {@link #initCause}.
-	 *
-	 * @param message the detail message. The detail message is saved for
-	 *                later retrieval by the {@link #getMessage()} method.
-	 */
 	public IllegalFieldSizeException(String message) {
 		super(message);
 	}
